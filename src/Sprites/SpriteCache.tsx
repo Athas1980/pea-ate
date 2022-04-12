7e386ebd4e5aedc09058fdf2dd49bfcd0d095cf0
achesonw <wesley.acheson@easypaymentgateway.com>
2022-04-12 18:44:52 +0100

    Work in progress

--- src/Sprites/SpriteCache.tsx
+++ src/Sprites/SpriteCache.tsx
@@ -1,9 +1,14 @@
-import { Component } from "react";
+import React, { Component, FC, useEffect } from "react";
 
 interface SpriteSheetData {
   width: number;
   height: number;
   data: Uint8Array;
+}
+
+interface SpriteCacheData {
+  spriteSheetData: SpriteSheetData
+  defaultPalette: Array<string> 
 }
 
 export class Sprites {
@@ -18,5 +23,49 @@ export class Sprites {
     const calculated = func(spriteNumber);
     this.sprites[spriteNumber] = calculated;
     return calculated;
+  }
+}
+
+export const SpriteCache : FC<SpriteCacheData> = 
+({defaultPalette, spriteSheetData}) =>{
+  const spriteCanvas = new OffscreenCanvas(spriteSheetData.width, spriteSheetData.height)
+  useEffect(
+    () => updateSpriteSheet(spriteSheetData.data)
+  )
+  function updateSpriteSheet(data: Uint8Array) {
+    if (spriteCanvas && data) {
+
+      const ctx = spriteCanvas.getContext("2d");
+      const imageData = ctx.getImageData(0, 0, spriteCanvas.width, spriteCanvas.height);
+      const byteData = imageData.data;
+      for (let i = 0; i < byteData.length; i += 4) {
+        const colour = paletteLookup(data[i / 4]);
+        byteData[i] = colour.r; // red
+        byteData[i + 1] = colour.g; // green
+        byteData[i + 2] = colour.b; // blue
+        byteData[i + 3] = colour.a;
+      }
+      ctx.putImageData(imageData, 0, 0);
+    }
+
+    function paletteLookup(index: number) {
+      const rgbPallete = defaultPalette.map(toRgba)
+      if (index && rgbPallete[index]) {
+        return rgbPallete[index]
+      }
+      return toRgba("000000")
+    }
+
+
+  }
+  return <span></span>
+}
+
+function toRgba(hex: string) {
+  return {
+    "r": parseInt(hex.substring(0, 2), 16),
+    "g": parseInt(hex.substring(2, 4), 16),
+    "b": parseInt(hex.substring(4, 6), 16),
+    "a": 255
   }
 }
