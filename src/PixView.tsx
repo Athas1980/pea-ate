ceee0d12fe9693ec66cb7f7f6504d51ea71fa9b3
Wesley Acheson <wesley.acheson@easypaymentgateway.com>
2022-05-05 21:21:11 +0100

    Temp work in progress

--- src/PixView.tsx
+++ src/PixView.tsx
@@ -1,23 +1,19 @@
 import React, { CSSProperties, FC, MouseEvent, useEffect } from 'react';
 import { Component, useRef } from 'react';
 
-type PixViewProps = {
+export type PixViewProps = {
   // using `interface` is also ok
   width: number;
   height: number;
-  zoom: number;
   palette: Array<string>;
   data: Uint8Array;
-
+  style?: React.CSSProperties
+  className?: string;
 };
 
-const PixView: FC<PixViewProps> = ({ width, height, zoom, data, palette }: PixViewProps) => {
+export const PixView: FC<PixViewProps> = ({ width, height, data, palette, style, className }: PixViewProps) => {
 
   const ref = useRef<HTMLCanvasElement>(null)
-  const style : CSSProperties = {
-    width: width * zoom,
-    height: height * zoom,
-  }
 
   useEffect(() => {
     if (ref.current && data) {
@@ -41,28 +37,34 @@ const PixView: FC<PixViewProps> = ({ width, height, zoom, data, palette }: PixVi
     if (index && rgbPallete[index]) {
       return rgbPallete[index]
     }
-    return toRgba("000000")
+    return toRgba("00000000")
   }
 
   function handleMouseMove(event: MouseEvent) {
     console.log({ x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY })
-    console.log({ x: Math.floor(event.nativeEvent.offsetX / zoom), y: Math.floor(event.nativeEvent.offsetY / zoom) })
+    let zoom = ref.current.offsetWidth / ref.current.width
+    console.log({zoom,  x: Math.floor(event.nativeEvent.offsetX / zoom), y: Math.floor(event.nativeEvent.offsetY / zoom) })
   }
 
+  const cssClass = className ? className : 'pixView'
   return (
-    <span className='pixView'>
-      <canvas ref={ref}
+      <canvas className={cssClass} ref={ref}
         height={height} width={width}
-        style={style} onMouseMove={handleMouseMove}></canvas>
-    </span>)
+        style={style} 
+        onMouseMove={handleMouseMove}></canvas>
+        )
 }
 
 function toRgba(hex: string) {
+  let a = 255
+  if(hex.length > 6) {
+    const a = parseInt(hex.substring(6, 8), 16)
+  }
   return {
     "r": parseInt(hex.substring(0, 2), 16),
     "g": parseInt(hex.substring(2, 4), 16),
     "b": parseInt(hex.substring(4, 6), 16),
-    "a": 255
+    "a": a
   }
 }
 
@@ -103,7 +105,9 @@ export class ZoomablePixView extends Component<ZoomablePixViewProperties, Zoomab
         <button className="btnZoomIn" onClick={this.zoomIn}>+</button>
         <button className="btnZoomOut" onClick={this.zoomOut} disabled={this.state.zoom <= 1}>-</button>
       </div>
-      <PixView data={this.props.data} height={this.props.height} width={this.props.width} palette={this.props.palette} zoom={this.state.zoom}></PixView>
+      <PixView data={this.props.data} height={this.props.height} width={this.props.width} palette={this.props.palette}
+        style={{width: this.props.width*this.state.zoom, height: this.props.height*this.state.zoom }}
+      ></PixView>
 
     </div>
 
--- src/SpriteEditor/SpriteEditor.tsx
+++ src/SpriteEditor/SpriteEditor.tsx
@@ -1,7 +1,9 @@
 import React, { useState} from 'react';
 import { FC } from "react";
 import { ColourChooser } from '../ColourChooser/ColourChooser';
-import { ZoomablePixViewProperties } from "../PixView";
+import { PixViewProps } from '../PixView';
+import { PixView } from "../PixView"
+import { ZoomablePixViewProperties }  from "../PixView";
 import { ZoomablePixView } from "../PixView"
 
 interface SpriteEditorProps {
@@ -23,22 +25,35 @@ export const SpriteEditor: FC<SpriteEditorProps> = (props: SpriteEditorProps) =>
     const [selectedColour, setSelectedColour] = useState(0)
     const [rows, setRows] = useState(3)
     const [cols, setCols] = useState(4)
+    const [selectedSprites, setSelectedSprites] = useState([0,1,2,3,16,17,18,19,32,33,34,35])
+    
     const editableSpriteArea: React.CSSProperties = {
+        gridColumnStart:1,
+        gridRowStart:1,
         display: 'grid',
         gridTemplateColumns: '1fr '.repeat(cols),
         gap: 0,
-        width: "40%",
         backgroundColor: "black"
     }
     const cells:JSX.Element[] = []
+
+    const editorData = new Uint8Array(rows*cols*64)
+
+    for (var i=0; i < rows*cols; i++) {
+        let destIdx =  
+    }
+
+
     for (var i=0; i< rows*cols; i++) {
         cells.push(<div style={editorCellStyle} key={i}>{i}</div>)
     }
-    
     return <div>
         <div style={{ backgroundColor: '#5F574F', boxSizing: 'border-box', padding: "10px" }}>
-            <div style={editableSpriteArea}>
-                {cells}
+            <div className='sprite-editor-wrapper'>
+                <div style={editableSpriteArea}>
+                    {cells}
+                </div>
+                <PixView className="pixEditor" width={cols*8} height={rows*8} palette={palette} data={new Uint8Array()}></PixView>
             </div>
             <ColourChooser palette={palette} handleSelection={setSelectedColour} selected={selectedColour} ></ColourChooser>
 
